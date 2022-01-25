28e8650937b6bec33265112952a51dd62b68be27
Sarthak Jha <sarthakjha94@gmail.com>
2022-01-25 10:34:41 +0530

    added 2 forms for signup and login

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,21 +1,59 @@
+import { useState } from "react";
+
 const Auth = () => {
+    
+    const [login, setLogin] = useState(true);
+
     const closeAuth = () => {
-        document.querySelector(".auth-quickview").remove();
+//        document.querySelector(".auth-quickview").remove();
+    }
+
+    const submitForm = () => {
+        // submit form on the basis of wether it is signup or login
+    }
+
+
+
+    const changeForm = (e) => {
+        e.preventDefault();
+        let isLogin = !login;
+        setLogin(isLogin);
     }
 
 	return (
 		<div className="auth-quickview" onClick={closeAuth}>
-            <form className="auth-form">
-                <div className="auth-block">
-                    <label htmlFor="username">Username: </label><br />
-                    <input type="text" placeholder="Username" name="username" />
-                </div>
-                <div className="auth-block">
-                    <label htmlFor="password">Password: </label><br />
-                    <input type="password" placeholder="password" name="password" />
-                </div>
-                <button>Login</button>
-            </form>
+            <button onClick={changeForm}>{login ? "LOGIN" : "SIGNUP"}</button>
+            <div className="both-forms">
+                {login ? 
+                    <form className="auth-form" method="POST" onSubmit={submitForm}>
+                        <div className="auth-block">
+                            <label htmlFor="username">Username: </label><br />
+                            <input type="text" placeholder="Username" name="username" />
+                        </div>
+                        <div className="auth-block">
+                            <label htmlFor="password">Password: </label><br />
+                            <input type="password" placeholder="password" name="password" />
+                        </div>
+                        <button>Login</button>
+                    </form>
+                :
+                    <form className="auth-form" method="POST" onSubmit={submitForm}>
+                        <div className="auth-block">
+                            <label htmlFor="username">Email: </label><br />
+                            <input type="text" placeholder="email@email.com" name="Email" />
+                        </div>
+                        <div className="auth-block">
+                            <label htmlFor="password">Password: </label><br />
+                            <input type="password" placeholder="password" name="password" />
+                        </div>
+                        <div className="auth-block">
+                            <label htmlFor="password">Password: </label><br />
+                            <input type="password" placeholder="password" name="password" />
+                        </div>
+                        <button>Sign Up</button>
+                    </form>
+                }
+            </div>
 		</div>
 	);
 };
