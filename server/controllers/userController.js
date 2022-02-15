1bbcb28bd68f846dbc95df029f6c75d14fb99f43
Sarthak Jha <sarthakjha94@gmail.com>
2022-02-15 13:38:51 +0530

    removed some minor bugs

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,33 +1,38 @@
 const User = require("../models/Users");
 
 const createUser = async (req, res) => {
-	try {
-		const { email, username, password } = req.body;
-		const dataToSave = { email, username, password };
-		const savingData = await User.create(dataToSave);
-		res.status(200).send(savingData);
-	} catch (err) {
-		res.status(404).send(err);
-	}
+    try {
+        const { email, username, password } = req.body;
+        const dataToSave = { email, username, password };
+        const savingData = await User.create(dataToSave);
+        res.status(200).send(savingData);
+    } catch (err) {
+        res.status(404).send(err);
+    }
 };
 
 const readUser = async (req, res) => {
-	try {
-		const { email, password } = req.body;
-		const userToLogin = { email, password };
-		const loginData = await User.read(userToLogin);
-		res.status(200).send(loginData);
-	} catch (err) {
-		res.status(404).send(err);
-	}
+    try {
+        const { email, password } = req.body;
+        const userToLogin = { email, password };
+        const loginData = await User.read(userToLogin);
+        console.log("asd", loginData);
+        if (loginData.status) {
+            res.cookie("username", "done");
+        }
+
+        res.status(200).send(loginData);
+    } catch (err) {
+        res.status(404).send(err);
+    }
 };
 
 const updateUser = (req, res) => {
-	//
+    //
 };
 
 const removeUser = (req, res) => {
-	//
+    //
 };
 
 module.exports = { createUser, readUser, updateUser, removeUser };
