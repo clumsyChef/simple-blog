ec158f67fa08f4206fca07c8db25dc41d33c78c5
Sarthak Jha <sarthakjha94@gmail.com>
2022-02-21 22:15:34 +0530

    removed git messages

--- server/server.js
+++ server/server.js
@@ -5,39 +5,31 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(
-    cors({
-        origin: "http://localhost:3000",
-        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    })
+	cors({
+		// origin: "http://localhost:3000",
+		// optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+	})
 );
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(function (req, res, next) {
-    res.header("Content-Type", "application/json;charset=UTF-8");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-// cookies
-=======
->>>>>>> 1bbcb28bd68f846dbc95df029f6c75d14fb99f43
-=======
->>>>>>> 1bbcb28bd68f846dbc95df029f6c75d14fb99f43
+// app.use(function (req, res, next) {
+// 	res.header("Content-Type", "application/json;charset=UTF-8");
+// 	res.header("Access-Control-Allow-Credentials", true);
+// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// 	next();
+// });
 
 // Connection to mongoose
 mongoose.connect(
-    "mongodb://localhost/blog-db",
-    () => {
-        console.log("CONNECTED Mongo");
-    },
-    (error) => {
-        console.error(error);
-    }
+	"mongodb://localhost/blog-db",
+	() => {
+		console.log("CONNECTED Mongo");
+	},
+	(error) => {
+		console.error(error);
+	}
 );
 
 /* ------- Require Routes Start --------- */
@@ -53,9 +45,9 @@ app.use("/blogs", blogs);
 /* ------- Require Routes End --------- */
 
 app.use((req, res) => {
-    res.status(404).json({ status: false });
+	res.status(404).json({ status: false });
 });
 
 app.listen(5000, () => {
-    console.log("CONNECTED");
+	console.log("CONNECTED");
 });
