5a95feeebdae4e9aa55db96a39992b8a103a4f4c
Sarthak Jha <sarthakjha94@gmail.com>
2022-03-11 12:10:03 +0530

    changed login button text to logout and vice-versa

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,64 +1,62 @@
 import axios from "axios";
-// import { useCookies } from "react";
 import { useCookies } from "react-cookie";
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 
 const Login = () => {
-	const [cookies, setCookie, removeCookie] = useCookies(["access_token"]);
-	const userContext = useContext(UserContext);
-	console.log(userContext);
-	const submitForm = (e) => {
-		e.preventDefault();
-		const [email, password] = [
-			document.querySelector("form#login-form [name='email']").value,
-			document.querySelector("form#login-form [name='password']").value,
-		];
+    const [cookies, setCookie, removeCookie] = useCookies(["access_token"]);
+    const userContext = useContext(UserContext);
+    const submitForm = (e) => {
+        e.preventDefault();
+        const [email, password] = [
+            document.querySelector("form#login-form [name='email']").value,
+            document.querySelector("form#login-form [name='password']").value,
+        ];
 
-		if (email.length < 1 || password.length < 1) {
-			alert("You must fill out form completely.");
-			return false;
-		}
+        if (email.length < 1 || password.length < 1) {
+            alert("You must fill out form completely.");
+            return false;
+        }
 
-		axios
-			.post("http://localhost:5000/users/login", {
-				email,
-				password,
-			})
-			.then((response) => {
-				if (response.status === 200) {
-					if (response.data.status) {
-						let loginToken = response.data.token;
-						let expires = new Date();
-						expires.setTime(expires.getTime() + 60 * 60 * 1000);
-						setCookie("access_token", loginToken, { path: "/", expires });
-						document.querySelector(".auth-quickview").classList.add("hide-auth");
-						console.log("DATA -> ", response.data.user, response.data.token);
-					} else {
-						if (response.data.message === "error") {
-							alert("Username Or Password is Wrong.");
-						}
-					}
-				}
-			});
-	};
-	return (
-		<>
-			<form className="auth-form" id="login-form">
-				<div className="auth-block">
-					<label htmlFor="username">Email: </label>
-					<br />
-					<input type="text" placeholder="email@example.com" name="email" />
-				</div>
-				<div className="auth-block">
-					<label htmlFor="password">Password: </label>
-					<br />
-					<input type="password" placeholder="password" name="password" />
-				</div>
-				<button onClick={submitForm}>Login</button>
-			</form>
-		</>
-	);
+        axios
+            .post("http://localhost:5000/users/login", {
+                email,
+                password,
+            })
+            .then((response) => {
+                if (response.status === 200) {
+                    if (response.data.status) {
+                        let loginToken = response.data.token;
+                        let expires = new Date();
+                        expires.setTime(expires.getTime() + 60 * 60 * 1000);
+                        setCookie("access_token", loginToken, { path: "/", expires });
+                        document.querySelector(".auth-quickview").classList.add("hide-auth");
+                        userContext.update({ authenticated: true });
+                    } else {
+                        if (response.data.message === "error") {
+                            alert("Username Or Password is Wrong.");
+                        }
+                    }
+                }
+            });
+    };
+    return (
+        <>
+            <form className="auth-form" id="login-form">
+                <div className="auth-block">
+                    <label htmlFor="username">Email: </label>
+                    <br />
+                    <input type="text" placeholder="email@example.com" name="email" />
+                </div>
+                <div className="auth-block">
+                    <label htmlFor="password">Password: </label>
+                    <br />
+                    <input type="password" placeholder="password" name="password" />
+                </div>
+                <button onClick={submitForm}>Login</button>
+            </form>
+        </>
+    );
 };
 
 export default Login;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,28 +1,33 @@
 import { Link } from "react-router-dom";
 import Auth from "./Auth";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 
 import { UserContext } from "../context/UserContext";
 
 const Navbar = () => {
-	const userContext = useContext(UserContext);
-	console.log(userContext);
+    const userContext = useContext(UserContext);
 
-	const [logged, setLogged] = useState(userContext["userData"]["authenticated"]);
+    const getAuth = () => {
+        document.querySelector(".hide-auth").classList.remove("hide-auth");
+        document.body.style.overflow = "hidden";
+    };
 
-	const getAuth = () => {
-		document.querySelector(".hide-auth").classList.remove("hide-auth");
-		document.body.style.overflow = "hidden";
-	};
+    const removeAuth = () => {
+        userContext.update({ authenticated: false });
+    };
 
-	return (
-		<nav className="navbar">
-			<Link to="/">Home</Link>
-			<Link to="/blogs">All Blogs</Link>
-			{logged ? <button>Log Out</button> : <button onClick={getAuth}>Login/Signup</button>}
-			<Auth />
-		</nav>
-	);
+    return (
+        <nav className="navbar">
+            <Link to="/">Home</Link>
+            <Link to="/blogs">All Blogs</Link>
+            {userContext["userData"]["authenticated"] ? (
+                <button onClick={removeAuth}>Log Out</button>
+            ) : (
+                <button onClick={getAuth}>Login/Signup</button>
+            )}
+            <Auth />
+        </nav>
+    );
 };
 
 export default Navbar;
